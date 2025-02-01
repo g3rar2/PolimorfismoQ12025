743d91191843767668745cbe15c80e64d674814b
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-01 15:12:34 -0600

    add classes Casa, Departamento, Terreno

---
+++ PolimorfismoQ12025/Departamento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorfismoQ12025
+{
+    internal class Departamento
+    {
+    }
+}
