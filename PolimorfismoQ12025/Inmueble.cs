c611f84c1b7f953ab03f6978972a1f68d32a9fa1
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-01 14:46:10 -0600

    add class inmueble

---
+++ PolimorfismoQ12025/Inmueble.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PolimorfismoQ12025
+{
+    public class Inmueble
+    {
+        public string TipoInmueble { get; set; }
+        public string Ubicacion {  get; set; }
+        public double AreaTerreno { get; set; }
+        public double Precio { get; set; }
+
+        public void vender()
+        {
+            Console.WriteLine($"{TipoInmueble}, con ubicacion en {Ubicacion}, con un area de terreno {AreaTerreno} mts2, ha sido vendido por el precio de L. {Precio}");
+        }
+    }
+}
